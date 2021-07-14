1ddae5e029fc8da956e1c5860a477590487d4f8c
tenbonks <thebentonks@gmail.com>
2021-07-14 20:01:50 +0100

    Added rocket noise, wired up to thruster code

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -9,15 +9,20 @@ public class Movement : MonoBehaviour
     [SerializeField] KeyCode inputLeft;
     [SerializeField] KeyCode inputRight;
 
+    // Thrust values set in unity inspector
     [SerializeField] float mainThrust = 10f;
     [SerializeField] float sideThrust = 0.3f;
 
     Rigidbody rb;
 
+    AudioSource myAudioSource;
+    bool audioPlaying;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>(); // Cache a reference of the rigidbody
+        myAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -34,6 +39,17 @@ public class Movement : MonoBehaviour
         if(Input.GetKey(inputThrust))
         {
             rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime); // Vector3.up shorthand for 0, 1, 0
+            
+            if (!audioPlaying)  // Only play the audio if it is not playing
+            {
+                myAudioSource.Play();
+                audioPlaying = true;
+            }
+        }
+        else    // Stop the audio and dont apply any force, as thrust is no longer being held down
+        {
+            myAudioSource.Stop();
+            audioPlaying = false;
         }
 
     }
@@ -55,7 +71,7 @@ public class Movement : MonoBehaviour
 
     void ApplyRotation(float rotationThisFrame)
     {
-        rb.freezeRotation = true;   // Freeze rotation, so manual input doesn't conflict with physics
+        rb.freezeRotation = true;   // Freeze rotation, the physics system would cause a bug with rotation when using transform
         transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
         rb.freezeRotation = false;  // Unfreeze rotation, let the physics system have a say on the situation again
     }

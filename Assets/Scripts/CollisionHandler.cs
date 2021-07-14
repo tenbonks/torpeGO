7b94360c51f07e503c3a1dad34132f59c8cfbcd4
tenbonks <thebentonks@gmail.com>
2021-07-14 23:19:59 +0100

    Added level progression, new level

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+
+    [SerializeField] float loadingDelay;
+
     void OnCollisionEnter(Collision other) 
     {
         switch (other.gameObject.tag)
@@ -9,16 +13,52 @@ public class CollisionHandler : MonoBehaviour
             case "Friendly":
                 Debug.Log("You've bumped into a FRIENDLY object");
                 break;
-            case "Fuel":
-                Debug.Log("Refuel");
-                break;
             case "Finish":
-                Debug.Log("You've reached the FINISH, yay!");
+                StartSuccessSequence();
                 break;
+            // case "Fuel":
+            //     Debug.Log("Refuel");
+            //     break;
             default:
-                Debug.Log("You hit an obstacle, BOOOOM!");
+                StartCrashSequence();
                 break;
 
         }   
+    }
+
+    void StartCrashSequence()   // Disable movement and reload the level
+    {
+        //Add sfx
+        //Add Particles
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", loadingDelay);  // String method name ref's are passed to Invoke, which is a bit loose bhole brah
+    }
+
+    void StartSuccessSequence()
+    {
+        //Add sfx
+        //Add Particles
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", loadingDelay);        
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex); 
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)  
+        {
+            // If they are on the last level, restart it from the first level (index 0)
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex); 
     }
 }
